27f5e5a0ea942c31bf93505b8e5ac4b4d72cdee1
vishvatejsurve <vishvatejsurve93@gmail.com>
2026-04-24 12:03:25 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/logistics_system/LogisticsSystemApplication.java
@@ -0,0 +1,13 @@
+package com.logistics_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsSystemApplication.class, args);
+	}
+
+}
